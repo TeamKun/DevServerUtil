b9c859b8605a0021e29b887e3cdee3811cdae344
maru-256 <maru043@docomo.ne.jp>
2021-06-13 04:04:12 +0900

    server.properties変更コマンドを実装

--- src/main/java/net/kunmc/lab/devserverutil/command/Message.java
+++ src/main/java/net/kunmc/lab/devserverutil/command/Message.java
@@ -4,4 +4,5 @@ import org.bukkit.ChatColor;
 
 public class Message {
     public static String opBlacklistUsage = ChatColor.RED + "usage: /dsu opBlacklist <add|remove> <name>";
+    public static String SVPropUsage = ChatColor.RED + "usage: /dsu serverProperties <configItem> <value>";
 }
--- src/main/java/net/kunmc/lab/devserverutil/command/SVPropCommand.java
+++ src/main/java/net/kunmc/lab/devserverutil/command/SVPropCommand.java
@@ -1,21 +1,70 @@
 package net.kunmc.lab.devserverutil.command;
 
-import net.kunmc.lab.devserverutil.DevServerUtil;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SVPropCommand implements SubCommand {
-    JavaPlugin plugin = DevServerUtil.getInstance();
+    private final Path propsFile = Paths.get("./server.properties");
 
     @Override
     public void run(CommandSender sender, String[] args) {
-        
+        if (args.length < 2) {
+            sender.sendMessage(Message.SVPropUsage);
+            return;
+        }
+
+        try {
+            List<String> propList = Files.readAllLines(propsFile);
+            int lineOffset = 0;
+            for (String prop : propList) {
+                if (prop.split("=")[0].equals(args[0])) {
+                    break;
+                }
+                lineOffset++;
+            }
+
+            if (lineOffset >= propList.size()) {
+                propList.add(args[0] + "=" + args[1]);
+            } else {
+                propList.set(lineOffset, args[0] + "=" + args[1]);
+            }
+
+            Files.write(propsFile, propList, StandardOpenOption.WRITE);
+        } catch (IOException e) {
+            e.printStackTrace();
+            sender.sendMessage(ChatColor.RED + "実行中にエラーが発生しました.");
+            return;
+        }
+
+        sender.sendMessage(ChatColor.GREEN + args[0] + "の値を" + args[1] + "に変更しました.\n適用にはサーバーの再起動が必要です.");
     }
 
     @Override
     public List<String> tabComplete(CommandSender sender, String[] args) {
-        return null;
+        if (args.length == 1) {
+            try {
+                return Files.readAllLines(propsFile).stream()
+                        .filter(x -> !x.startsWith("#"))
+                        .map(x -> x.split("=")[0])
+                        .collect(Collectors.toList());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (args.length == 2) {
+            return Collections.singletonList("<value>");
+        }
+
+        return Collections.emptyList();
     }
 }
